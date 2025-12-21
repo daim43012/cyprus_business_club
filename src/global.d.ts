3df9d8e19ba20090c4a2fd4043432c4b92a23aa8
daim43012 <vgatsko.21@gmail.com>
2025-12-22 02:57:40 +0300

    add global types for paypal

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,20 @@
+/// <reference types="@sveltejs/kit" />
+
 declare module "leaflet";
+
+declare global {
+  interface Window {
+    paypal?: {
+      Buttons: (options: {
+        createOrder?: (data: any, actions: any) => Promise<string> | string;
+        onApprove?: (data: any, actions: any) => Promise<void> | void;
+        onError?: (err: any) => void;
+        onCancel?: (data: any) => void;
+      }) => {
+        render: (container: string | HTMLElement) => void;
+      };
+    };
+  }
+}
+
+export {};
